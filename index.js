7576c03278cd9dbba427ca6689fd9bba601b1180
manishaa8981 <manishaa8981@gmail.com>
2026-03-30 19:25:51 +0545

    added frontend render website

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin:[ 'http://localhost:5173',
+      "https://estate-hub-frontend.onrender.com",
+],
+credentials: true }))
 app.use(express.json())
 
 app.get('/', (req, res) => res.json({ message: 'EstateHub API is running' }))
